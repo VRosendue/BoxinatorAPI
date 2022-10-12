346223cca51682479ca198fb44b9de821d000195
MGylden <99180585+MGylden@users.noreply.github.com>
2022-10-12 18:47:36 +0200

    Update UsersController.java

--- BoxinatorVim/src/main/java/com/example/demo/controller/UsersController.java
+++ BoxinatorVim/src/main/java/com/example/demo/controller/UsersController.java
@@ -2,9 +2,7 @@ package com.example.demo.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.GetMapping;
-=======
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
->>>>>>> working-Endpoints
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.models.Users;
@@ -30,15 +27,7 @@ public class UsersController {
 
 	@Autowired
 	UserService userService;
-	
-	@Autowired
-<<<<<<< HEAD
-	private UserRepository userRepository;
-	
-=======
-	UserRepository userRepo;
->>>>>>> working-Endpoints
-	
+
 	/*
 	 * @GetMapping("AllUsers2") public List<User> getAllUsers2(){ return
 	 * (List<User>) this.userRepo.findAll(); }
@@ -49,7 +38,6 @@ public class UsersController {
 		return userService.getUserById(userId);
 	}
 	
-<<<<<<< HEAD
 	/*
 	 * @GetMapping("shipments") public List<Shipments> getAllShipments() { return
 	 * this.shipmentRepository.findAll(); }
@@ -58,10 +46,6 @@ public class UsersController {
 	 * this.countryRepository.findAll(); }
 	 */
 	
-	}
-	
-	
-=======
 	@GetMapping()
 	public ResponseEntity<List<Users>> getAllUsers() {
 		return userService.getAllUsers();
@@ -81,28 +65,3 @@ public class UsersController {
 		return userService.updateUser(newUser, id);
 	}
 }
->>>>>>> working-Endpoints
-
-/*
- * @GetMapping("/hello") // GET: localhost:8080/api/v1/test/1 public
- * ResponseEntity<String> path(@RequestParam String first_name) { return
- * ResponseEntity.ok().body(String.valueOf(first_name)); }
- * 
- * // Extracting a query string
- * 
- * @GetMapping // GET: localhost:8080/api/v1/test?key=value public
- * ResponseEntity<String> query(@RequestParam String first_name) { return
- * ResponseEntity.ok().body(first_name); }
- * 
- * // Extracts the User-Agent header
- * 
- * @GetMapping("header") // GET: localhost:8080/api/v1/header public
- * ResponseEntity<String> headers(@RequestHeader("User-Agent") String agent) {
- * return ResponseEntity.ok().body(agent); }
- * 
- * // Extracts the body
- * 
- * @GetMapping("body") // GET: localhost:8080/api/v1/body public
- * ResponseEntity<String> body(@RequestBody String value) { return
- * ResponseEntity.ok().body(value); } }
- */
