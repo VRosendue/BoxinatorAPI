ed316a31ce7ea9391ce8eb5b30cdd4548446adf6
MGylden <mathias@gylden.net>
2022-10-17 16:12:11 +0200

    jwt

--- BoxinatorVim/src/main/java/com/example/demo/controller/CountryController.java
+++ BoxinatorVim/src/main/java/com/example/demo/controller/CountryController.java
@@ -1,7 +1,11 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties.Jwt;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.models.Country;
 import com.example.demo.services.CountryService;
+
+
 import static com.example.demo.controller.ControllerHelper.BASE_URI_V1;
 
 import java.util.List;;
@@ -26,26 +32,29 @@ public class CountryController {
 	private CountryService countryService;
 
 	@GetMapping
-	public ResponseEntity<List<Country>> getAllCountries() {
+	public ResponseEntity<List<Country>> getAllCountries(@AuthenticationPrincipal Jwt jwt) {
 		return countryService.getAllCountries();
 	}
 	
 	@GetMapping("/{CountryId}")
-	public ResponseEntity<Country> getCountryById(@PathVariable long CountryId) {
+	public ResponseEntity<Country> getCountryById(@PathVariable long CountryId, @AuthenticationPrincipal Jwt jwt) {
 		return countryService.getCountryById(CountryId);
 	}
 
 	@PostMapping
+	@PreAuthorize("hasRole(ADMIN)")
 	public ResponseEntity<Country> createCountry(@RequestBody Country newCountry){
 		return countryService.createCountry(newCountry);
 	}
 
 	@DeleteMapping("/{id}")
+	@PreAuthorize("hasRole(ADMIN)")
 	public ResponseEntity<Country> deleteCountry(@PathVariable(value="id")Long id){
 		return countryService.deleteCountry(id);
 	}
 	
 	@PutMapping("/{id}")
+	@PreAuthorize("hasRole(ADMIN)")
 	public ResponseEntity<Country>updateCountry(@PathVariable(value="id")Long id, @Validated @RequestBody Country newCountry){
 		return countryService.updateCountry(newCountry, id);
 	}
