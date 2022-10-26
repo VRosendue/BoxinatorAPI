4b8d694fd02c07a38bb1f7c15ab813f99b2f7c3a
MGylden <99180585+MGylden@users.noreply.github.com>
2022-10-26 12:34:32 +0200

    Small changes
    changes to controller
    changes to security config

--- BoxinatorVim/src/main/java/com/example/demo/controller/ShipmentController.java
+++ BoxinatorVim/src/main/java/com/example/demo/controller/ShipmentController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,8 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.models.Shipments;
-import com.example.demo.repository.ShipmentRepository;
-
 import com.example.demo.services.ShipmentService;
 
 import static com.example.demo.controller.ControllerHelper.BASE_URI_V1;
