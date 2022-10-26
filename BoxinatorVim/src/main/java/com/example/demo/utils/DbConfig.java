44e73ca340e8695c353717adc6ac608a5de46e2d
MGylden <mathias@gylden.net>
2022-10-26 12:18:31 +0200

    DbConfig

---
+++ BoxinatorVim/src/main/java/com/example/demo/utils/DbConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo.utils;
+
+import javax.activation.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+public class DbConfig {
+
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+
+	@Bean
+	public HikariDataSource dataSource() {
+		HikariConfig config = new HikariConfig();
+		config.setJdbcUrl(dbUrl);
+		return new HikariDataSource(config);
+	}
+}
